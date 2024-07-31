137f7492655aecfe65704b1d0b7d9c7cdfd49e93
liandrazakilaputri <145527693+liandrazakilaputri@users.noreply.github.com>
2024-07-31 13:21:42 +0700

    Pertemuan 2 nomor 2 Variabel pada Java

---
+++ Project1soal2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project1soal2;
+
+/**
+ *
+ * @author liandra
+ */
+public class Project1soal2 {
+
+    public static String Makanan;
+    public static final String Minuman = "Jus Jambu";
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Makanan = "Ayam Geprek ";//Mengisi Value pada variable Makanan
+        System.out.println("Makanan Favorite Saya " + Makanan);//Memanggil Variable Makanan
+        System.out.println("Minuman Favorite Saya " 
+                + Minuman);//Memanggil Variable Minuman   
+    }
+}
