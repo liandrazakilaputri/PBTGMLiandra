fa7a41200e0296d3e844d1d6c73a7f093f7d7b87
liandrazakilaputri <145527693+liandrazakilaputri@users.noreply.github.com>
2024-08-12 12:06:20 +0700

    Pertemuan4soal1 Operator Pada Java

---
+++ Pertemuan4soal1/src/main/java/com/mycompany/pertemuan4soal1/Pertemuan4soal1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4soal1;
+
+/**
+ *
+ * @author liandra
+ */
+public class Pertemuan4soal1 {
+
+    public static void main(String[] args) {
+       int angkaA = 10;
+       int angkaB = 3;
+System.out.println("Hasil Pertambahan A+B= 13 " + 
+(angkaA + angkaB) );
+System.out.println("Hasil Pengurangan A-B= 7 " + 
+(angkaA - angkaB) );
+System.out.println("Hasil Perkalian A*B= 30 " + 
+(angkaA * angkaB) );
+System.out.println("Hasil Pembagian A/B= 3,333333 " + 
+(angkaA / angkaB) );
+    }
+}
