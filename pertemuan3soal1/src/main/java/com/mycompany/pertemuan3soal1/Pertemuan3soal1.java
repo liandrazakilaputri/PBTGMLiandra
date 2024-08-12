3f2acc24bf14e789bc3a57b44c4a63b63c2c7d52
liandrazakilaputri <145527693+liandrazakilaputri@users.noreply.github.com>
2024-08-12 11:41:04 +0700

    Pertemuan3soal1 Tipe Data Pada Java

---
+++ pertemuan3soal1/src/main/java/com/mycompany/pertemuan3soal1/Pertemuan3soal1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3soal1;
+
+/**
+ *
+ * @author liandra
+ */
+public class Pertemuan3soal1 {
+
+    public static void main(String[] args) {
+       // TODO code application logic here
+ int a = 9;
+ float f = 899;
+ char ch = 'A';
+ boolean b = false;
+ String string = "Hello World";
+ System.out.println("int Varl " + a);
+ System.out.println("float Var2 " + f);
+ System.out.println("char Var3 " + ch);
+ System.out.println("boolean Var4 " + b);
+ System.out.println("string Var5 " + string);
+    }
+}
