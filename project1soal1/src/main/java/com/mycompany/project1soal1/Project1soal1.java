0c6a6ca6dd6e85629f7fd614d463f5480cd3e356
liandrazakilaputri <145527693+liandrazakilaputri@users.noreply.github.com>
2024-08-12 11:37:18 +0700

    Pertemuan 2 soal 1 VARIABEL PADA JAVA

---
+++ project1soal1/src/main/java/com/mycompany/project1soal1/Project1soal1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project1soal1;
+
+/**
+ *
+ * @author liandra
+ */
+public class Project1soal1 {
+
+    public static void main(String[] args) {
+        int a=50; 
+float b=a; 
+System.out.println(a); 
+System.out.println(b); 
+    }
+}
