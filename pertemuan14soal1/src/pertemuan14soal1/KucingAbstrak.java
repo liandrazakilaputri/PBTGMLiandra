afb3b512f0a6cbb004b5f4903c966aabfb6fb70d
liandrazakilaputri <145527693+liandrazakilaputri@users.noreply.github.com>
2024-08-27 20:47:10 +0700

    Pertemuan14soal1 ABSTRACK CLASS INTERFACE

---
+++ pertemuan14soal1/src/pertemuan14soal1/KucingAbstrak.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan14soal1;
+
+/**
+ *
+ * @author liandra
+ */
+public class KucingAbstrak extends AbstrakHewan {
+    @Override
+    public void suara() {
+        System.out.println(" Suara kucing mengeong");
+    
+    }
+}  
+
---
+++ pertemuan14soal1/src/pertemuan14soal1/Pertemuan14soal1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan14soal1;
+
+/**
+ *
+ * @author liandra
+ */
+public class Pertemuan14soal1 {
+    public static void main(String[] args) {
+         // TODO code application logic here
+        AbstrakHewan objek = new KucingAbstrak();
+        objek.suara();
+        objek.suara2();
+    }
+    
+}
