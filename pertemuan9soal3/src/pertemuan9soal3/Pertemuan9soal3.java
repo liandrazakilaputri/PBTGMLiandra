efca2f507e4bf96193e51639df509dea5161c9dd
liandrazakilaputri <145527693+liandrazakilaputri@users.noreply.github.com>
2024-08-27 20:30:07 +0700

    Pertemuan9soal3 Mengontrol Aliran Program Perulangan While, DO WHILE

---
+++ pertemuan9soal3/src/pertemuan9soal3/Pertemuan9soal3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan9soal3;
+
+/**
+ *
+ * @author liandra
+ */
+public class Pertemuan9soal3 {
+   
+    public static void main(String[] args) {
+       int i = 1;
+       while (i < 6) {
+           System.out.println(i);
+           i++;
+       }
+    }    
+}
