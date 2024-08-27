150086b39c06a088a678c2e3f6123e0b42a0e34b
liandrazakilaputri <145527693+liandrazakilaputri@users.noreply.github.com>
2024-08-27 20:20:57 +0700

    Pertemuan7soal1 Mengontrol Aliran Program Pernyataan SWITCH ..CASE

---
+++ pertemuan7soal1/src/pertemuan7soal1/Pertemuan7soal1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan7soal1;
+
+/**
+ *
+ * @author liandra
+ */
+public class Pertemuan7soal1 {
+
+    public static void main(String[] args) {
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Sabtu");
+                break;
+            case 2:
+                System.out.println("Minggu");
+                break;
+        
+        }
+    }
+    
+}
